2c1bb629cb58957219d2c1eb2c5f765b1f72743c
shreeharsha-factly <harsha@factly.in>
2020-05-08 16:39:29 +0530

    prefix added to table name

--- model/setup.go
+++ model/setup.go
@@ -39,6 +39,13 @@ func SetupDB() {
 	}
 	fmt.Println("connected to database ...")
 
+	DB.SingularTable(true)
+
+	// adding default prefix to table name
+	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
+		return "dp_" + defaultTableName
+	}
+
 	// db migrations
 	DB.AutoMigrate(
 		&Currency{},
@@ -60,22 +67,22 @@ func SetupDB() {
 	)
 
 	// Adding foreignKey
-	DB.Model(&Payment{}).AddForeignKey("currency_id", "currencies(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&Membership{}).AddForeignKey("user_id", "users(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&Membership{}).AddForeignKey("plan_id", "plans(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&Membership{}).AddForeignKey("payment_id", "payments(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&Product{}).AddForeignKey("currency_id", "currencies(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&Product{}).AddForeignKey("status_id", "statuses(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&Product{}).AddForeignKey("product_type_id", "product_types(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&ProductTag{}).AddForeignKey("tag_id", "tags(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&ProductTag{}).AddForeignKey("product_id", "products(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&ProductCategory{}).AddForeignKey("category_id", "categories(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&ProductCategory{}).AddForeignKey("product_id", "products(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&Cart{}).AddForeignKey("user_id", "users(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&CartItem{}).AddForeignKey("cart_id", "carts(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&CartItem{}).AddForeignKey("product_id", "products(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&Order{}).AddForeignKey("payment_id", "payments(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&Order{}).AddForeignKey("cart_id", "carts(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&OrderItem{}).AddForeignKey("product_id", "products(id)", "RESTRICT", "RESTRICT")
-	DB.Model(&OrderItem{}).AddForeignKey("order_id", "orders(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&Payment{}).AddForeignKey("currency_id", "dp_currency(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&Membership{}).AddForeignKey("user_id", "dp_user(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&Membership{}).AddForeignKey("plan_id", "dp_plan(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&Membership{}).AddForeignKey("payment_id", "dp_payment(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&Product{}).AddForeignKey("currency_id", "dp_currency(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&Product{}).AddForeignKey("status_id", "dp_status(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&Product{}).AddForeignKey("product_type_id", "dp_product_type(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&ProductTag{}).AddForeignKey("tag_id", "dp_tag(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&ProductTag{}).AddForeignKey("product_id", "dp_product(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&ProductCategory{}).AddForeignKey("category_id", "dp_category(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&ProductCategory{}).AddForeignKey("product_id", "dp_product(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&Cart{}).AddForeignKey("user_id", "dp_user(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&CartItem{}).AddForeignKey("cart_id", "dp_cart(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&CartItem{}).AddForeignKey("product_id", "dp_product(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&Order{}).AddForeignKey("payment_id", "dp_payment(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&Order{}).AddForeignKey("cart_id", "dp_cart(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&OrderItem{}).AddForeignKey("product_id", "dp_product(id)", "RESTRICT", "RESTRICT")
+	DB.Model(&OrderItem{}).AddForeignKey("order_id", "dp_order(id)", "RESTRICT", "RESTRICT")
 }
