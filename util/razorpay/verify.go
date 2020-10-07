52da695c6de5aefc890ed99e9a720a82828a7030
hetjagani <hetpatel572@gmail.com>
2020-10-07 13:01:29 +0530

    resolved linting issue

--- util/razorpay/verify.go
+++ util/razorpay/verify.go
@@ -12,7 +12,10 @@ import (
 // VerifySignature verifies razorpay payment signature
 func VerifySignature(orderID, paymentID, signature string) bool {
 	h := hmac.New(sha256.New, []byte(config.RazorpaySecret))
-	h.Write([]byte(fmt.Sprint(orderID, "|", paymentID)))
+	_, err := h.Write([]byte(fmt.Sprint(orderID, "|", paymentID)))
+	if err != nil {
+		return false
+	}
 
 	generated := hex.EncodeToString(h.Sum(nil))
 
