c479a85a693bef50788883f870019b90e8f55e57
shreeharsha-factly <harsha@factly.in>
2021-05-29 19:13:40 +0530

    fix cors issue

--- action/routes.go
+++ action/routes.go
@@ -32,7 +32,6 @@ import (
 	"github.com/factly/x/loggerx"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"github.com/go-chi/cors"
 	"github.com/spf13/viper"
 	httpSwagger "github.com/swaggo/http-swagger"
 )
@@ -46,17 +45,6 @@ func GetCommonRouter() chi.Router {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Heartbeat("/ping"))
-
-	r.Use(cors.Handler(cors.Options{
-		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"*"},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	}))
 
 	sqlDB, _ := model.DB.DB()
 	healthx.RegisterRoutes(r, healthx.ReadyCheckers{
